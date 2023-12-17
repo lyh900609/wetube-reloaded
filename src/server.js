2821375961917d5dd4a39768ab53468cb25f5cd1
lyh900609 <136784627+lyh900609@users.noreply.github.com>
2023-12-17 23:48:40 +0900

    middleWare_D12.17_C3.6

--- src/server.js
+++ src/server.js
@@ -4,15 +4,42 @@ const PORT = 4000;
 
 const app = express(); //*서버이름, express funcion 사용 express application 생성
 
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`); 
+  next(); //*완료가 되고 난 후에 handleHome을 진행
+};
+
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("<h1>Not Allowed</h1>");
+  }
+  console.log("Allowed!!")
+  next();
+};
+
 const handleHome = (req, res) => {
+  console.log("now in homePage!")
   return res.end(); //*request 종료. request가 왔으면 response를 return해줘야함.
-}
-const handleLogin = (req, res) => {
-  return res.send("Login here.");
+};
+
+const handleProtected = (req, res) => {
+  return res.send("welco to the Private lounge!");
 }
 
-app.get("/", handleHome); //*누군가 root 페이지로 get request를 보냄
-app.get("/login", handleLogin);
+app.use(logger);
+app.use(privateMiddleware);
+app.get("/", handleHome);
+app.get("/protected", handleProtected);
+ //*누군가 root 페이지로 get request를 보냄
+
+ const globalRouter = express.Router();
+ const userRouter = express.Router();
+ const videoRouter = express.Router();
+
+ app.use("/", globalRouter);
+ app.use("/vidoes", videoRouter);
+ app.use("user", userRouter);
 
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`)
 
